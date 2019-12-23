5a1bf759af263e216cf9bd055187d2bc41e78ab1
Yasin Kaymaz <yasinkaymaz@github.com>
2019-12-23 14:38:56 -0500

    pipe

--- Snakemake/Run_pipe.sh
+++ Snakemake/Run_pipe.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
 WORK_DIR=$1
-cores=12
-
-
+SnakeFile=$2
 cd $WORK_DIR
+cores=12
 dir=`pwd`
 SAMPLE_NAME=`basename $dir`
 
 ln -s ~/codes/scRNAseq_Benchmark/Snakemake/Scripts ./
 #Modify the config file
 
-sbatch -p holy-info \
+rm -r .snakemake/
+
+sbatch -p serial_requeue \
 -n ${cores} \
--w holy2c0529 \
 --mem 256000 \
 -t 6-0:00 \
 --job-name=BM_"$SAMPLE_NAME" \
 -e err_"$SAMPLE_NAME".%j.txt \
--o out_"$SAMPLE_NAME".%j.txt \
-bash Scripts/Run_SnakeMake.sh $cores
+-o out_"$SAMPLE_NAME".%j.txt Scripts/Run_SnakeMake.sh $SnakeFile
--- Snakemake/Scripts/Run_SnakeMake.sh
+++ Snakemake/Scripts/Run_SnakeMake.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
+SnakeFile=$1#~/codes/scRNAseq_Benchmark/Snakemake/Snakefile
 
-cores=$1
-
+cores=12
 . /n/home13/yasinkaymaz/miniconda3/etc/profile.d/conda.sh
 #conda env export -n R3.3 > R3.6.yml
 conda activate R3.6
 
 snakemake -k \
-  -s ~/codes/scRNAseq_Benchmark/Snakemake/Snakefile \
+  -s $SnakeFile \
   --configfile config.yml \
   --use-singularity \
   --use-conda \
